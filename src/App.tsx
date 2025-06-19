a88af09246d4ff0805922874a9e595e30234f932
Oseni Oluwabunmi <osenibunmi2023@gmail.com>
2025-06-19 14:22:48 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>hello world</h1>
+      <Button>hello there</Button>
+    </>
+  )
+}
+
+export default App
